49d6c66f509369f7793b89a03a978cc272b5c942
dkingdon <daniel.kingdon@gmail.com>
2016-10-31 22:26:39 -0700

    couldnt firgure this one out, needed the solution.

--- challenges/letter-count.js
+++ challenges/letter-count.js
@@ -12,7 +12,40 @@
     "e": 1
   }
   ```
+*/
 
+/* spent a LOT of time on this and couldnt get it to work. needed to look at the solution, understand now how it is done, but still higher than my level. :(  was working with this option below but couldnt get it to work. */
+
+// var letterCount = function(word) {
+//   var obj = {};
+//   var arr = word.split('') // ['a','p','p','l','e']
+//
+//   arr.forEach(function(char){
+//       if (obj[char] = obj[char]) {
+//           obj[char] += 1;
+//       }
+//       else {
+//           obj[char] = 1;
+//       }
+//       return obj;
+//   });
+// }
+
+
+function letterCount(string) {
+ var count = {};
+ string.split('').forEach(function(el) {
+   count[el] = count[el] ? count[el] + 1 : 1;
+ });
+ return count;
+}
+letterCount("apple");
+
+
+  // use .split to create array of objects, create
+  // Look up dynamically adding key value pairs brackets
+// maybe a while loop?
+/*
   Bonuses
   - Make sure that lower case letters and upper case letters count for the same character.
   - Ignore spaces, special characters, and punctuation.
